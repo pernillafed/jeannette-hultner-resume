01786fa21c0516962f9ce76c4d96e85f6784d132
Pernilla <coolanilla@hotmail.com>
2023-03-13 14:32:18 +0100

    Added styling to presentation page

--- src/pages/presentation/Presentation.jsx
+++ src/pages/presentation/Presentation.jsx
@@ -6,7 +6,7 @@ const Presentation = () => {
     return (
         <div className={styles.presentation}>
             <h1>Presentation</h1>
-            <video width="640" controls>
+            <video width="100%" controls>
                 <source src={PresentationVideo} type="video/mp4" />
             </video>
             <p>{presentationText.introduction}</p>
