96292176ce8f3c1ae003b3e9205853f4b1e6ad8a
Pernilla <coolanilla@hotmail.com>
2023-03-14 18:55:28 +0100

    Added more styling to picture modal and added outside click hook

--- src/pages/pictures/picture-modal/PictureModal.jsx
+++ src/pages/pictures/picture-modal/PictureModal.jsx
@@ -1,11 +1,17 @@
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from "react";
+import useOutsideClick from "../../../hooks/useOutsideClick";
 import styles from "./PictureModal.module.css";
 
 const PictureModal = ({ imagePathProp, setShowModal }) => {
+    const modalRef = useRef();
+
+    useOutsideClick(() => setShowModal(false), modalRef);
+
     return (
         <div className={styles.pictureModal}>
-            <div>
+            <div ref={modalRef}>
                 <FontAwesomeIcon icon={faXmark} onClick={() => setShowModal(false)} />
                 <img src={imagePathProp} alt="Jeannette Hultner" />
             </div>
