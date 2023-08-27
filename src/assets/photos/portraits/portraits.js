4b0f04675ce1d83dc4acfd58af3b40a5b2b10354
Pernilla <coolanilla@hotmail.com>
2023-08-27 14:59:02 +0200

    Updated portrait photos

--- src/assets/photos/portraits/portraits.js
+++ src/assets/photos/portraits/portraits.js
@@ -4,7 +4,6 @@ import portrait3 from "./portrait3.JPG";
 import portrait4 from "./portrait4.JPG";
 import portrait5 from "./portrait5.JPG";
 import portrait6 from "./portrait6.jpg";
-import portrait7 from "./portrait7.jpg";
 
 export default [
     portrait1,
@@ -13,5 +12,4 @@ export default [
     portrait4,
     portrait5,
     portrait6,
-    portrait7
 ];
