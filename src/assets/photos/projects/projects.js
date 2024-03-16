413d5af943d2f4b2f93c2562104d2c05c5372822
Pernilla <coolanilla@hotmail.com>
2024-03-16 17:29:32 +0100

    remove incorrect image

--- src/assets/photos/projects/projects.js
+++ src/assets/photos/projects/projects.js
@@ -7,7 +7,7 @@ import project06 from "./project06.jpg";
 import project07 from "./project07.JPG";
 import project08 from "./project08.JPG";
 import project09 from "./project09.JPG";
-import project10 from "./project10.JPG";
+// import project10 from "./project10.JPG";
 import project11 from "./project11.jpg";
 import project12 from "./project12.jpg";
 import project13 from "./project13.jpg";
@@ -26,7 +26,7 @@ export default [
     project07,
     project08,
     project09,
-    project10,
+    // project10,
     project11,
     project12,
     project13,
